8a4a61585e714497ceb28949323b332272f43e85
mspake <morgana.spake@gmail.com>
2019-04-23 12:37:23 -0700

    constructor built and superagent.get working

--- server.js
+++ server.js
@@ -10,12 +10,36 @@ app.use(express.static('./public')); //for the purposes of our site, public is t
 app.use(express.urlencoded({ extended: true }));
 app.set('view-engine', 'ejs');
 
+//===============================
+// Constructor
+//===============================
+
+function Book(book) {
+  this.title = book.volumeInfo.title || 'Title not found';
+  this.authors = book.volumeInfo.authors || 'Could not find Author';
+  this.description = book.volumeInfo.description || 'No description given.';
+  this.photo = book.volumeInfo.imageLinks.thumbnail;
+}
+
+//===============================
+//===============================
+
+//superagent.get(url).then( result => {
+//  new Book(result.body.items[0])
+// })
 
 app.get('/', (request, response) => {
   response.render('pages/index.ejs');
 });
 
-app.post('/search', (request, response) => {
+app.post('/searches', (request, response) => {
+  const query = request.body.search;
+  console.log(query);
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+  superagent.get(url).then(result => {
+    const bookResults = result.body.items.slice(0, 10);
+    console.log(bookResults.length);
+  });
   response.send('Search Received');
 });
 
@@ -23,5 +47,7 @@ app.post('/search', (request, response) => {
 app.get('/test', (request, response) => {
   response.render('pages/index.ejs');
 });
+
+//===============================
 
 app.listen(PORT, () => { console.log(`App is up on PORT ${PORT}`) });
