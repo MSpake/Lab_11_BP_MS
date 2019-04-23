b0350746f8eee19dd08137b91dfc2f0a62ca151c
mspake <morgana.spake@gmail.com>
2019-04-23 11:07:28 -0700

    form added and working

--- server.js
+++ server.js
@@ -3,24 +3,25 @@
 const PORT = process.env.PORT || 3000;
 
 const express = require('express');
-
 const superagent = require('superagent');
 
 const app = express();
-
 app.use(express.static('./public')); //for the purposes of our site, public is the root folder
-
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.urlencoded({ extended: true }));
 app.set('view-engine', 'ejs');
 
-app.get('/', (request, response)=>{
+
+app.get('/', (request, response) => {
   response.render('pages/index.ejs');
 });
-// below test renders page 
-app.get('/test', (request, response)=>{
-    response.render('pages/index.ejs');
-  });
 
-app.listen(PORT, ()=> {console.log(`App is up on PORT ${PORT}`)});
+app.post('/search', (request, response) => {
+  response.send('Search Received');
+});
 
+// below test renders page
+app.get('/test', (request, response) => {
+  response.render('pages/index.ejs');
+});
+
+app.listen(PORT, () => { console.log(`App is up on PORT ${PORT}`) });
